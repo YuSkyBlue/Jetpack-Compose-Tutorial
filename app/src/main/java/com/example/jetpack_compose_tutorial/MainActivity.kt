19da376b8149b7487a71bb0012bb979992cb61ab
ck <dbcjdrb1997@gmail.com>
2022-05-16 23:28:47 +0900

    <first commit> Row - horizontalArrangement - verticalAlignment
    Column
    - verticalArrangement
    - horizontalAlignment
    
    Stud
    - Arrangement
    - Alignment

---
+++ app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.jetpack_compose_tutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpack_compose_tutorial.ui.theme.JetpackComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Row(
+                modifier = Modifier
+                    .width(300.dp)
+                    .fillMaxHeight(0.7f)
+                    .background(Color.Green),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "hello")
+                Text(text = "world")
+                Text(text = "number 3")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTutorialTheme {
+        Text(text = "Hello")
+        Text(text = "world")
+    }
+}
